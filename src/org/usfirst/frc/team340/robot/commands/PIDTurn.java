627095ce993c06a0df3938674c03b3ed2723e7ff
wundrweapon <whhacker.dcx@gmail.com>
2017-03-27 20:38:37 -0400

    Unnecessary imports are unnecessary

--- src/org/usfirst/frc/team340/robot/commands/PIDTurn.java
+++ src/org/usfirst/frc/team340/robot/commands/PIDTurn.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team340.robot.commands;
 
 import org.usfirst.frc.team340.robot.Robot;
 
-import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 
 /**
